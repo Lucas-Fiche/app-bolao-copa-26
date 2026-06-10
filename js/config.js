fb9b612bb29a604d03838492046b0a687e26a1db
Lucas Fiche <120349280+Lucas-Fiche@users.noreply.github.com>
2026-06-09 23:13:45 -0300

    Atualiza o link do script

--- js/config.js
+++ js/config.js
@@ -3,4 +3,4 @@
 // Cole abaixo a URL do seu App da Web do Google Apps Script
 // (Implantar > Nova implantação > App da Web > copiar a URL /exec).
 // ============================================================
-const API_URL = 'https://script.google.com/macros/s/COLE_SEU_ID_DE_IMPLANTACAO_AQUI/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbxnK5mq9b6xro_HEfbhr8QhG7ZYUdU_F7QwsUT1hU4awMb6ZLCQO89lH4S1oCNLrkODcw/exec';
